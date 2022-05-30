52a96bc42d6207b8b1e3ae6d1cbc6a02e371ecc9
VuDej <drvu47@gmail.com>
2022-05-30 05:31:17 +0200

    created static controller and set static view and route. Generated react component greeting

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'static/index'
   namespace :v1, defaults: { format: 'json' } do
     get 'greetings', to: 'greetings#index'
   end
+  root "static#index"
 end
